9e5a9388d7d89005443d103d20f6fc076f6fcd0b
Hunter Heston <huntersamuelheston@gmail.com>
2023-05-22 11:41:40 -0700

    creating short urls on the home page button click

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,14 +10,45 @@ export default function Shrink() {
       }
     >
       <motion.button
-        //  className="btn btn-outline hover:bg-black border-0 hover:text-white text-black font-bold text-2xl uppercase"
         className={
           styles.button +
           " btn btn-outline text-black font-bold text-2xl uppercase"
         }
+        onClick={async () => {
+          const clipboardContents = await getContentsOfClipBoard();
+          console.log(clipboardContents);
+          if (isValidWebAddress(clipboardContents)) {
+            const result = await fetch(`/api/new?url=${clipboardContents}`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+            const data = await result.json();
+            console.log(data);
+            if (result.status === 200) {
+              console.log(`${location.host}/${data.slug}`);
+              navigator.clipboard.writeText(`${location.host}/${data.slug}`);
+            }
+          }
+        }}
       >
         <span>Shrink Clipboard</span>
       </motion.button>
     </div>
   );
+}
+
+// function gets what ever string is currently in the clipboard
+async function getContentsOfClipBoard(): Promise<string> {
+  return await navigator.clipboard.readText();
+}
+
+async function isValidWebAddress(url: string): Promise<boolean> {
+  try {
+    new URL(url);
+    return true;
+  } catch (_) {
+    return false;
+  }
 }
