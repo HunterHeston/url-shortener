60fe0b76ef18e2740619b15edb06e1c109c6ca6c
Hunter Heston <huntersamuelheston@gmail.com>
2023-05-22 08:44:07 -0700

    use db logic from db submodule

---
+++ pages/api/new.ts
@@ -0,0 +1,63 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// initialize a prisma client
+import { PrismaClient } from "../../submodules/database/node_modules/@prisma/client";
+import { newShrinkURL } from "@/submodules/database/lib/shrinkURL";
+const prisma = new PrismaClient();
+
+type Data = {
+  slug?: string;
+  url?: string;
+  errorMessage?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // get url from query
+  const { url } = req.query;
+
+  if (!isValidWebAddress(url as string)) {
+    return res.status(400).json({ errorMessage: "Invalid URL" });
+  }
+
+  // create new database row
+  const slug = generateRandomSlug(5);
+  const result = await newShrinkURL({
+    slug,
+    url: url as string,
+  });
+
+  // some failure occurred while creating database entries.
+  if (result instanceof Error) {
+    return res.status(500).json({ errorMessage: "Internal Server Error" });
+  }
+
+  // success send back slug and url
+  res.status(200).json({
+    url: result.url,
+    slug: result.slug,
+  });
+}
+
+function generateRandomSlug(n: number) {
+  const characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const charactersLength = characters.length;
+  let result = "";
+  for (let i = 0; i < n; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function isValidWebAddress(url: string): boolean {
+  try {
+    new URL(url);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
