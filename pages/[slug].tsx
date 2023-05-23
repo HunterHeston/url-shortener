a10c99cf40a67f983d333c5e3bfb8c08968e1602
Hunter Heston <huntersamuelheston@gmail.com>
2023-05-23 16:14:46 -0700

    Just call the prisma function directly.

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,3 +1,4 @@
+import { getShrinkURLBySlug } from "@/submodules/database/lib/ShrinkURL";
 import { GetServerSidePropsContext } from "next";
 
 type RedirectProps = {
@@ -13,16 +14,10 @@ export async function getServerSideProps(
 ): Promise<any> {
   const slug = context.query.slug;
   try {
-    const result = await fetch(
-      `${context.req.headers.host}/api/get?slug=${slug}`
-    );
-    const data = await result.json();
+    const result = await getShrinkURLBySlug(slug as string);
 
-    if (result.status === 200) {
-      context.res.writeHead(301, { Location: data.url });
-      context.res.end();
-    } else {
-    }
+    context.res.writeHead(301, { Location: result.url });
+    context.res.end();
   } catch (err) {
     console.log(err);
   }
