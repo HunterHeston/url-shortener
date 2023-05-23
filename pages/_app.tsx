c11aa45d6fb6ebb087028862e534b312c9ca8f9f
Hunter Heston <huntersamuelheston@gmail.com>
2023-05-23 09:35:57 -0700

    Add new Font.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
+
+const mont = Montserrat({
+  weight: "500",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div data-theme="black">
+    <div data-theme="black" className={mont.className}>
       <Component {...pageProps} />
     </div>
   );
