72c961624b2270cba7c38ecb7946bfffd7ecd022
Hunter Heston <huntersamuelheston@gmail.com>
2023-05-22 20:14:59 -0700

    Weird open animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { motion } from "framer-motion";
 import styles from "./index.module.css";
+import { useState } from "react";
 
 export default function Shrink() {
+  const [shortURL, setShortURL] = useState("");
   return (
     <div
       className={
@@ -20,21 +22,50 @@ export default function Shrink() {
           if (isValidWebAddress(clipboardContents)) {
             const result = await fetch(`/api/new?url=${clipboardContents}`, {
               method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
             });
             const data = await result.json();
             console.log(data);
             if (result.status === 200) {
-              console.log(`${location.host}/${data.slug}`);
-              navigator.clipboard.writeText(`${location.host}/${data.slug}`);
+              const url = `http://${location.host}/${data.slug}`;
+              navigator.clipboard.writeText(url);
+              setShortURL(url);
             }
           }
         }}
       >
         <span>Shrink Clipboard</span>
       </motion.button>
+      <UrlView open={shortURL !== ""} url={shortURL} />
+    </div>
+  );
+}
+
+type UrlViewProps = {
+  open: boolean;
+  url: string;
+};
+export function UrlView({ open, url }: UrlViewProps) {
+  return (
+    <div>
+      {open && (
+        <motion.div
+          initial={{ scale: 0, borderRadius: "100%" }}
+          animate={{ scale: 1, borderRadius: "0%" }}
+          transition={{ duration: 0.4 }}
+          className="flex items-center justify-center text-white text-2xl"
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            background: "rgba(0, 0, 0, 1)",
+            zIndex: 9999,
+          }}
+        >
+          <div>{url}</div>
+        </motion.div>
+      )}
     </div>
   );
 }
